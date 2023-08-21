abd419c2c11cf3474c32297b4e6f7215ec6d07d2
bon4ppetit <bon4ppetit@gmail.com>
2023-08-21 17:49:44 +0300

    Add goal panel

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/vue3';
 import FormQuestion from "@/Pages/Home/FormQuestion.vue";
 import {ref} from "vue";
 import StructurePanel from "@/Pages/Home/StructurePanel.vue";
+import GoalPanel from "@/Pages/Home/GoalPanel.vue";
 
 const props = defineProps([
     'basicInfoSmoke'
@@ -22,9 +23,12 @@ let viewForms = props.basicInfoSmoke !== false ? ref(false) : ref(true);
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-8">
                     <FormQuestion v-if="viewForms === true" :viewForms="viewForms"></FormQuestion>
                     <StructurePanel v-else-if="viewForms === false" :basicInfoSmoke="basicInfoSmoke"></StructurePanel>
+                </div>
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" v-if="viewForms === false">
+                  <GoalPanel :basicInfoSmoke="basicInfoSmoke"></GoalPanel>
                 </div>
             </div>
         </div>
---
+++ resources/js/Pages/Home/GoalPanel.vue
@@ -0,0 +1,28 @@
+<script setup>
+  const props = defineProps(['basicInfoSmoke'])
+</script>
+
+<template>
+  <div class="p-5 flex justify-between">
+      <div>
+        <h2 class="text-white font-bold text-2xl flex">
+          Ваша копилка:
+          <span class="font-bold text-violet-700 block ml-3">
+            {{basicInfoSmoke['money_saving']}}руб.
+          </span>
+        </h2>
+        <span class="text-zinc-400 text-center text-sm">
+        У вас ещё нету цели для накопления. <br>Советуем поставьте её, чтобы накопленные деньги потратились не даром.
+      </span>
+      </div>
+    <div class="text-white flex-auto flex items-center justify-center">
+      <button class="px-4 py-2 bg-violet-900/80 rounded-lg border-violet-700 border-2">
+        Поставить цель
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
